d70f93a35cb5cf33b8e73f3e736128c3719e84f1
Brazenbillygoat <hyrum.butler3@gmail.com>
2020-10-11 14:42:51 -0400

    Wrote two first-class functions

--- index.js
+++ index.js
@@ -10,4 +10,19 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+const greet = (time) => {
+  let currentHour = time.split(':')
+  if (currentHour[0] < 12) {
+    return "Good Morning"
+  } else if (currentHour[0] >= 12 && currentHour[0] < 17) {
+    return "Good Afternoon"
+  } else {
+    return "Good Evening"
+  }
+}
+
 /* Write your implementation of displayMessage() */
+const displayMessage = (message) => {
+  let greet = document.querySelector('#greeting');
+  greet.innerHTML = message;
+}
